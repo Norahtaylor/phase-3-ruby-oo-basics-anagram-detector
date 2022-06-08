d75f75e00d20fced798883e309bb466c8d92c558
Norah Taylor <emt368@gmail.com>
2022-06-08 17:16:57 -0400

    commit tree ab18f579533a845848581832211526fe2e4c085b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :name
+    
+    def initialize(word)
+        @name = word 
+    end 
+
+    def match(array)
+        array = []
+    end 
+    # def match(array)
+    #     array.select {|x| x.split("").sort == @name.split("").sort}
+    #   end   
+end 
+
+# listen = Anagram.new("listen")
+# listen.match(w[enlists google inlets banana])
